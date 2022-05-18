eed3d5a65ac535bd7d4cfc0db239312e5bfb0382
miraz <105309402+Miraz661@users.noreply.github.com>
2022-05-19 00:45:12 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var showB=document.querySelector("#showResult");
+function clearScr()
+{
+    showB.innerHTML=0;
+}
+function num0()
+{
+    showB.innerHTML=0;
+}
+function num1()
+{
+    showB.innerHTML=1;
+}
+showB.innerHTML=1000;
